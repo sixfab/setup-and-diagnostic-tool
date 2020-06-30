f8c5c6a13a33b55eeb1147d6e9d7c3e36bcaceef
Ensar Karabudak <ensarkarabudak@gmail.com>
2020-06-30 22:14:01 +0300

    update setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sixfab-diagnostic-tool',
-    version='0.0.1',
+    version='0.0.2',
     author='Ensar Karabudak',
     author_email='ensarkarabudak@gmail.com',
     description='Sixfab Diagnostic Tool',
